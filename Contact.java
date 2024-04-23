f92675c5276a69eed4ce426dc30ab95bea52360e
Viktor Golev <vwpiligrim@gmail.com>
2024-04-23 11:10:28 +0300

    rewrote the Contact class logic

--- Contact.java
+++ Contact.java
@@ -1,70 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Contact {
 
     private String fullName;
-    private List<String> phoneNumbers;
-    private String email;
-    private String additionalInfo;
+    private String phoneNumber;
 
-    public Contact(String fullName, String phoneNumber, String email, String additionalInfo) {
+    public Contact(String fullName, String phoneNumber) {
         this.fullName = fullName;
-        this.phoneNumbers = new ArrayList<>();
-        this.phoneNumbers.add(phoneNumber);
-        this.email = email;
-        this.additionalInfo = additionalInfo;
-    }
-
-    public Contact(String fullName, List<String> phoneNumbers, String email, String additionalInfo) {
-        this.fullName = fullName;
-        this.phoneNumbers = phoneNumbers;
-        this.email = email;
-        this.additionalInfo = additionalInfo;
+        this.phoneNumber = phoneNumber;
     }
 
     public String getFullName() {
         return fullName;
     }
 
-    public List<String> getPhoneNumbers() {
-        return phoneNumbers;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getAdditionalInfo() {
-        return additionalInfo;
-    }
-
-    public void addPhoneNumber(String phoneNumber) {
-        phoneNumbers.add(phoneNumber);
-    }
-
-    public void edit(Contact contact) {
-        if (contact.getFullName() != null) {
-            fullName = contact.getFullName();
-        }
-        if (contact.getPhoneNumbers() != null) {
-            phoneNumbers = contact.getPhoneNumbers();
-        }
-        if (contact.getEmail() != null) {
-            email = contact.getEmail();
-        }
-        if (contact.getAdditionalInfo() != null) {
-            additionalInfo = contact.getAdditionalInfo();
-        }
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     @Override
     public String toString() {
         return "Contact{" +
                 "fullName='" + fullName + '\'' +
-                ", phoneNumbers=" + phoneNumbers +
-                ", email='" + email + '\'' +
-                ", additionalInfo='" + additionalInfo + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
                 '}';
     }
 }
